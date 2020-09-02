569fa7cd4f95e8182cada6db39dde51a6de7a7f2
harrykim14 <67398691+harrykim14@users.noreply.github.com>
2020-09-02 12:32:19 +0900

    Node.js Studying 20.09.02
    chapter07-1

--- ch02/ch07_module01.js
+++ ch02/ch07_module01.js
@@ -4,4 +4,20 @@ function showUser() {
   return user1.getUser().name + ", " + user1.group.name;
 }
 
-console.log("사용자 정보 : %s", showUser());
+console.log("사용자 정보1 : %s", showUser());
+
+// const user2 = require("./user2");
+
+// function showUser2() {
+//   return user2.getUser().name + ", " + user2.group.name;
+// }
+
+// console.log("사용자 정보2 : %s", showUser2());
+
+const user3 = require("./user3");
+
+function showUser3() {
+  return user3.getUser().name + ", " + user3.group.name;
+}
+
+console.log("사용자 정보3 : %s", showUser3());
