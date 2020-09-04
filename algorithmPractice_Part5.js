c33bf99893713cdd468b8c73c3e9309b4e7177c6
harrykim14 <67398691+harrykim14@users.noreply.github.com>
2020-09-05 01:58:00 +0900

    Algorithm Studying 20.09.04
    until 70

--- algorithmPractice_Part5.js
+++ algorithmPractice_Part5.js
@@ -150,3 +150,161 @@ function solution2(n) {
 
 const handshake = 59;
 console.log(solution2(handshake));
+
+console.log(
+  "68. -------------------------------------------------------------"
+);
+
+const busSchedule = ["12:30", "13:20", "14:13"];
+const inputTime = "12:40";
+
+const time2num = busSchedule.map((t) => {
+  return parseInt(t.split(":").join(""));
+});
+const inputTime2num = parseInt(inputTime.split(":").join(""));
+const remainTime = [];
+
+for (time of time2num) {
+  if (time - inputTime2num < 0) {
+    remainTime.push("지나갔습니다.");
+  } else {
+    remainTime.push(Math.abs(time - inputTime2num));
+  }
+}
+// console.log(remainTime);
+
+const remainTime2Str = remainTime.map((t) => {
+  if (!isNaN(t)) {
+    let hour = Math.floor(t / 60);
+    let min = Math.floor(t % 60);
+    return `${hour < 10 ? "0" + hour : hour}시간 ${
+      min < 10 ? "0" + min : min
+    }분`;
+  } else {
+    return t;
+  }
+});
+
+console.log(remainTime2Str);
+
+console.log(
+  "69. -------------------------------------------------------------"
+);
+
+function primeList(n) {
+  let sieve = [];
+  for (let i = 2; i < n; i++) {
+    sieve.push(true);
+  }
+
+  let m = parseInt(n ** 0.5, 10);
+
+  for (let i = 2; i < m + 1; i++) {
+    if (sieve[i] == true) {
+      for (let j = i * i; j < n; j += i) {
+        sieve[j] = false;
+      }
+    }
+  }
+
+  let prime = [];
+  for (let i = 2; i < n; i++) {
+    if (sieve[i] == true) {
+      prime.push(i);
+    }
+  }
+  return prime;
+}
+
+// let prime = [];
+// let primeCheck = true;
+
+// for (let i = 2; i < 100; i++) {
+//   for (let j = 2; j < i; j++) {
+//     if (i % j == 0) {
+//       primeCheck = false;
+//     }
+//   }
+//   if (primeCheck) {
+//     prime.push(i);
+//   }
+//   primeCheck = true;
+// }
+// console.log(prime);
+
+// let count69 = 0;
+// const inputNumber = 100;
+// let goldbachArr = [];
+// for (let n of prime) {
+//   if (prime.include(inputNumber - n)) {
+//     console.log(n, inputNumber - n);
+//   }
+//   if (count69 > parseInt(prime.length / 2, 10)) {
+//     break;
+//   }
+//   count69++;
+// }
+
+function goldbachNum(num) {
+  const nPrimeList = primeList(num + 1);
+
+  let count69 = 0;
+  let goldbachArr = [];
+  for (let n of nPrimeList) {
+    if (nPrimeList.includes(num - n)) {
+      goldbachArr.push([n, num - n]);
+    }
+    if (count69 > parseInt(nPrimeList.length / 2, 10)) {
+      console.log(n, num - n);
+      break;
+    }
+    count69++;
+  }
+}
+
+goldbachNum(10000);
+
+console.log(
+  "70. -------------------------------------------------------------"
+);
+
+const matrix1 = [
+  [1, 2],
+  [2, 4],
+];
+const matrix2 = [
+  [1, 0],
+  [0, 3],
+];
+
+function solution3(a, b) {
+  let c = [];
+  // 만들어질 행렬을 초기화
+  const len = a[0].length;
+  // 행렬 연산 기준이 될 행렬의 길이를 먼저 구함
+  if (len == b.length) {
+    // 행렬 연산은 기준 행렬의 행, 곱할 열이 같아야 가능
+    for (let i = 0; i < len; i++) {
+      let row = [];
+      for (let j = 0; j < len; j++) {
+        let x = 0;
+        for (let k = 0; k < len; k++) {
+          x += a[i][k] * b[k][j];
+          // i = 0 번째 루프
+          // a[0][0] * b[0][0] + a[0][1] * b[1][0];
+          // a[0][1] * b[1][0] + a[0][1] * b[1][1];
+        }
+        row.push(x);
+        // 행 연산(각 a[i] 관련 연산)이 여기서 끝나니까 push 해 주기
+      }
+      c.push(row);
+      // 연산이 완료된 row를 push
+    }
+  } else {
+    return -1;
+  }
+  return c;
+}
+
+const newMatrix = solution3(matrix1, matrix2);
+console.log(newMatrix);
